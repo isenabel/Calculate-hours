79c1657441081f6b79b43dd7e5451d39abbc57fd
Isen Abel Liu Then <isenabel@hotmail.com>
2024-07-23 00:58:28 -0400

    make partners randomizer

--- random_name_picker.py
+++ random_name_picker.py
@@ -4,22 +4,40 @@ import random
 import sys
 
 print("""Use "," to separate each name.
-      Example: Marcos, Pedro, Juan, Mateo
-      """)
+Example: Marcos, Pedro, Juan, Mateo
+""")
 
 def main():
-    """The main function
-    """
+    """The main function"""
 
+    iterations: int = 0
     values = input("Insert values: ")
     new_list = [value.strip() for value in values.split(",")]
-    iterations: int = 0
+    print()
+    print("1) Single randomizer", "2) Make partners randomizer.", sep='\n')
+    while True:
+        option = input("Select option: ")
+        print()
 
-    while len(new_list):
-        iterations += 1
-        print(f"{iterations}- {new_list.pop(random.randrange(len(new_list)))}")
+        if option in {'1', '1)', 'one', 'One'}:
+            while len(new_list):
+                iterations += 1
+                print(f"{iterations}- {new_list.pop(random.randrange(len(new_list)))}")
 
-    print('-' * 10)
+            print('-' * 20)
+            break
+
+        if option in {'2', '2)', 'two', 'Two'}:
+            while len(new_list):
+                if iterations % 2 == 0:
+                    print(f"{new_list.pop(random.randrange(len(new_list)))}", end=" <==> ")
+                else:
+                    print(f"{new_list.pop(random.randrange(len(new_list)))}")
+
+                iterations += 1
+
+            print('-' * 20)
+            break
 
 main()
 
